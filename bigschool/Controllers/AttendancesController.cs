bdaaf8bea2d70a0d422a2d3db8544f4f86c1b3b3
Hien0000 <mhkhanhhien0206@gmail.com>
2022-04-02 19:16:37 +0700

    update 4

--- bigschool/Controllers/AttendancesController.cs
+++ bigschool/Controllers/AttendancesController.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using HttpPostAttribute = System.Web.Http.HttpPostAttribute;
+//using HttpPostAttribute = System.Web.Http.HttpPostAttribute;
 
 namespace bigschool.Controllers
 {
