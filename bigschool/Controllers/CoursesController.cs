c96c204ce645877e59ef9c8a72712337e0234c8f
Hien0000 <mhkhanhhien0206@gmail.com>
2022-04-04 09:26:21 +0700

    update 5

--- bigschool/Controllers/CoursesController.cs
+++ bigschool/Controllers/CoursesController.cs
@@ -69,5 +69,37 @@ namespace bigschool.Controllers
             };
             return View(viewModel);
         }
+
+        [Authorize]
+        public ActionResult Mine ()
+        {
+            var userId = User.Identity.GetUserId();
+            var courses = _dbContext.Courses
+                .Where(c => c.LecturerId == userId && c.DateTime > DateTime.Now)
+                .Include(l => l.Lecturer)
+                .Include(c => c.Category)
+                .ToList();
+            return View(courses);
+
+        }
+
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var course = _dbContext.Courses.Single(c => c.Id == id && c.LecturerId == userId);
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList(),
+                Date = course.DateTime.ToString("dd/M/yyyy"),
+                Time = course.DateTime.ToString("HH:mm"),
+                Category = course.CategoryId,
+                Place = course.Place,
+            };
+            return View("Create", viewModel);
+
+        }
+        
+
     }
 }
